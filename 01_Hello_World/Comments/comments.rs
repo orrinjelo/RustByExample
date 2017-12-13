f0d6515ca8bf5c8fe057089c3fdc9e22989b82ac
Tyler Park <tyler.park@fortemtech.com>
2017-12-13 13:26:36 -0700

    First two chapters and an example mini-game.

---
+++ 01_Hello_World/Comments/comments.rs
@@ -0,0 +1,7 @@
+// 1.1. Comments
+fn main()
+{
+    // Nobody here but us comments!
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+}
---
+++ 01_Hello_World/hello.rs
@@ -0,0 +1,11 @@
+// 1. Hello World
+// This is the main function
+fn main() {
+    // The statements here will be executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+
+    // This was added in the Activity
+    println!("I'm a Rustacean!");
+}
